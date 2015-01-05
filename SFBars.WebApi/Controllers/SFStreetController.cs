e69f03ea1e1fce90a6770e39b3c2edf17eeff20f
Jackie Trillo <jackietrillo@hotmail.com>
2015-01-04 19:11:31 -0800

    SFStreet/Bar REST Get

--- SFBars.WebApi/Controllers/SFStreetController.cs
+++ SFBars.WebApi/Controllers/SFStreetController.cs
@@ -24,13 +24,43 @@ namespace SFBars.Api.Controllers
 		public IEnumerable<SFStreetModel> Get()
 		{
 			IEnumerable<SFStreet> sfStreets = _SfStreetService.GetAll();
-			IEnumerable<SFStreetModel> sfStreetModels = sfStreets.Select( s => new SFStreetModel{
+
+			List<SFStreetModel> sfStreetModels = new List<SFStreetModel>();
+
+			foreach (var s in sfStreets)
+			{
+				var sfStreetModel = new SFStreetModel{
 				SFStreetId = s.SFStreetId,
 				Name = s.Name,
+				Descrip = s.Descrip,
 				Latitude = s.Latitude,
 				Longitude = s.Longitude,
-				ImageUrl= s.ImageUrl				
-			}).ToList<SFStreetModel>();
+				ImageUrl = s.ImageUrl};
+
+				sfStreetModel.SFBars = new List<SFBarModel>();
+
+				foreach (var bar in s.SFBars)
+				{
+					var sfBarModel = new SFBarModel
+					{
+						SFBarId = bar.SFBarId,
+						Name = bar.Name,
+						Descrip = bar.Descrip,
+						Latitude = bar.Latitude,
+						Longitude = bar.Longitude,
+						ImageUrl = bar.ImageUrl,
+						Address = bar.Address,
+						Phone = bar.Phone,
+						WebsiteUrl = bar.WebsiteUrl,
+						FacebookUrl = bar.FacebookUrl,
+						YelpUrl = bar.YelpUrl,
+					};
+
+					sfStreetModel.SFBars.Add(sfBarModel);
+				}
+
+				sfStreetModels.Add(sfStreetModel);
+			}
 
 			return sfStreetModels;
 		}
@@ -42,6 +72,7 @@ namespace SFBars.Api.Controllers
 			SFStreetModel model = new SFStreetModel {
 				SFStreetId = entity.SFStreetId,
 				Name = entity.Name,
+				Descrip = entity.Descrip,
 				Latitude = entity.Latitude,
 				Longitude = entity.Longitude,
 				ImageUrl = entity.ImageUrl				
@@ -49,15 +80,25 @@ namespace SFBars.Api.Controllers
 
 			model.SFBars = new List<SFBarModel>();
 
-			foreach (var SFBar in entity.SFBars)
-			{
-				SFBarModel barModel = new SFBarModel { SFBarId = SFBar.SFBarId, Name = SFBar.Name };
+			foreach (var bar in entity.SFBars) {
+				SFBarModel barModel = new SFBarModel {
+					SFBarId = bar.SFBarId,
+					Name = bar.Name ,
+					Descrip = bar.Descrip,
+					Latitude = bar.Latitude,
+					Longitude = bar.Longitude,
+					ImageUrl = bar.ImageUrl,
+					Address = bar.Address,
+					Phone = bar.Phone,
+					WebsiteUrl = bar.WebsiteUrl,
+					FacebookUrl = bar.FacebookUrl,
+					YelpUrl = bar.YelpUrl,
+				};
 				
 				model.SFBars.Add(barModel);
 			}
 		
 			return model;
-
 		}
 
 		// POST api/values
