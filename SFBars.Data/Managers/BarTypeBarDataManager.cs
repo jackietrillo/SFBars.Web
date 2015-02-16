9ab96d418c7cd36e9f0d1e58855e5af3570fa6ec
Jackie Trillo <jackietrillo@hotmail.com>
2015-02-15 17:01:44 -0800

    Refactoring for better pe

---
+++ SFBars.Data/Managers/BarTypeBarDataManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Bars.Core.Domain;
+
+namespace Bars.Data
+{
+	internal sealed class BarTypeBarDataManager : Repository<BarTypeBar>
+	{
+		public BarTypeBarDataManager(BarsDbContext context)
+			: base(context)
+		{	
+			//
+		}
+
+		public IQueryable<BarTypeBar> GetBarTypeBarTable()
+		{
+			return this.Table.AsQueryable();
+		}
+	}
+}
